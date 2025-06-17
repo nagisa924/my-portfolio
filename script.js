22503cc23086381caf70ebab41eb54b8442ad563
nagisa924 <nagisa.wd@gmail.com>
2025-06-17 21:25:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+<script>
 $(function(){
   $('#js-hamburger').on('click', function(){
     $('#header__nav').toggleClass('open');
     $(this).toggleClass('open');
   });
+
   setTimeout(function(){
     $(".hero-text h1").fadeIn(1000);
   }, 1000);
 });
+</script>
+
 

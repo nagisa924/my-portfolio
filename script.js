ff706faca283e1ee852a5836eab2980f9f67e727
nagisa924 <nagisa.wd@gmail.com>
2025-06-17 20:56:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
- <script>
-        $(document).ready(function () {
-            $('#js-hamburger').on('click', function () {
-                // console.log("ハンバーガーメニューがクリック");
-                $('#header__nav').toggleClass('open');
-                $('#js-hamburger').toggleClass('open');
-                $('.header__nav-list').toggleClass('open');
-
-                // console.log("ナビゲーションのクラス:", $("#header__nav").attr("class"));
-                // console.log("ハンバーガーのクラス:", $("#js-hamburger").attr("class"));
-
-            });
-        });
-    </script>
-
-
-
-$(document).ready(function () {
-    setTimeout(function () {
-        $(".hero-text h1").fadeIn(1000);
-    }, 1000);
+$(function(){
+  $('#js-hamburger').on('click', function(){
+    $('#header__nav').toggleClass('open');
+    $(this).toggleClass('open');
+  });
+  setTimeout(function(){
+    $(".hero-text h1").fadeIn(1000);
+  }, 1000);
 });
+

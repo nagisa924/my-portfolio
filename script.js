cb95a8aac9f590405348c291aa75ab9536bfd263
nagisa924 <nagisa.wd@gmail.com>
2025-06-17 21:44:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 $(function(){
   $('#js-hamburger').on('click', function(){
-    $('#header__nav').toggleClass('open');
+    $('#js-nav').toggleClass('open');
     $(this).toggleClass('open');
   });
-
   setTimeout(function(){
-    $(".hero-text h1").fadeIn(1000);
-  }, 1000);
+    $(".hero-text h1").css({opacity:0, display:'block'}).animate({opacity:1},1000);
+  },1000);
 });
+
 
 
 

d732c1f2f3c42b05c64f78dda33eb277c9e70ef3
nagisa924 <nagisa.wd@gmail.com>
2025-06-17 14:49:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // jQuery を使ったハンバーガーメニュー切り替え
-$(function () {
-    $('#hamburger').on('click', function () {
-        $('#nav-menu').toggleClass('active');
-    });
+$('.hamburger').on('click', function () {
+    $('.header__nav-list').toggleClass('active');
 });
 
 
+
 $(document).ready(function () {
-    // 1秒後に h1 要素をフェードイン
     setTimeout(function () {
-        $(".hero-text h1").fadeIn(1000); // 1000 = 1秒かけてフェードイン
-    }, 1000); // 表示開始まで1秒待つ
+        $(".hero-text h1").fadeIn(1000);
+    }, 1000);
 });
